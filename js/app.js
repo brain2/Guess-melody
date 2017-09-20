d592fe2a0168a027e002ef73c234377f51046e59
Dmitry Sokolyansky <sokoldp3@gmail.com>
2017-09-20 10:17:10 +0300

    implement functional wrappers

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 import Welcome from './screen/welcome';
 import Game from './screen/game';
 import Stats from './screen/stats';
-
+import Model from './model';
+import guessMelodyAdapter from './data/guessMelodyAdapter';
+import statsAdapter from './data/statsAdapter';
 
 const ControllerID = {
   WELCOME: ``,
@@ -9,17 +11,34 @@ const ControllerID = {
   STATS: `stats`
 };
 
-const getControllerIDFromHash = (hash) => {
+const Url = {
+  READ: `https://intensive-ecmascript-server-btfgudlkpi.now.sh/guess-melody/questions`,
+  STATS: `https://intensive-ecmascript-server-btfgudlkpi.now.sh/guess-melody/stats/vk402841`
+};
 
+const getControllerIDFromHash = (hash) => {
   return (hash.replace(`#`, ``).split(`=`));
 };
 
-class Application {
+export default class Application {
   constructor() {
+
+    this.model = new class extends Model {
+      get urlRead() {
+        return Url.READ;
+      }
+
+      get urlStats() {
+        return Url.STATS;
+      }
+    }();
+  }
+
+  setup(data) {
     this.routes = {
-      [ControllerID.WELCOME]: Welcome,
-      [ControllerID.GAME]: Game,
-      [ControllerID.STATS]: Stats
+      [ControllerID.WELCOME]: () => new Welcome(this),
+      [ControllerID.GAME]: () => new Game(this.model, data, this),
+      [ControllerID.STATS]: (stats) => new Stats(stats, this)
     };
     window.onhashchange = () => {
       this.changeController(getControllerIDFromHash(location.hash));
@@ -27,16 +46,43 @@ class Application {
   }
 
   changeController([route = ``, stats]) {
-    const Controller = this.routes[route];
-    if (Controller === Stats) {
-      new Controller(JSON.parse(stats)).init();
-    } else {
-      new Controller().init();
-    }
+    const controller = this.routes[route];
+    controller(stats ? JSON.parse(decodeURIComponent(stats)) : ``).init();
   }
 
-  init() {
-    this.changeController(getControllerIDFromHash(location.hash));
+  init(app) {
+    this.preloader();
+
+    this.loadQuestions()
+      .then((data) => this.setup(data, app))
+      .then(() => this.changeController(getControllerIDFromHash(location.hash)))
+      .catch(window.console.error);
+  }
+
+  preloader() {
+    new Welcome().init();
+    document.querySelector(`button`).style.display = `none`;
+  }
+
+  loadQuestions() {
+    this.changeUrlRead(Url.READ);
+
+    return this.model.load(guessMelodyAdapter);
+  }
+
+  loadStats() {
+    this.changeUrlRead(Url.STATS);
+
+    return this.model.load(statsAdapter);
+  }
+
+  changeUrlRead(url) {
+    Object.defineProperty(this.model, `urlRead`, {
+      configurable: true,
+      get() {
+        return url;
+      }
+    });
   }
 
   showWelcome() {
@@ -48,10 +94,6 @@ class Application {
   }
 
   showStats(stats) {
-    location.hash = `${ControllerID.STATS}=${stats}`;
+    location.hash = stats ? `${ControllerID.STATS}=${encodeURIComponent(JSON.stringify(stats))}` : `${ControllerID.STATS}`;
   }
 }
-
-const app = new Application();
-
-export default app;
